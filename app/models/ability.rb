18c80981a22e3b4182a5c242efc8a5847dc677c9
kibirige <kibirigejohn005@gmail.com>
2023-01-05 00:04:04 +0300

    configure cancancan ability file so that only authorized users deletes an inventory

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,3 +1,10 @@
 class Ability
   include CanCan::Ability
+
+  def initialize(user)
+    can :read, Inventory, public: true
+
+    return unless user.present?
+    can :manage, :all, user:user
+  end
 end
