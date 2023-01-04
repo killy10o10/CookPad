3d260f11a014741a2cfa26c00c7a788e58625bd6
kibirige <kibirigejohn005@gmail.com>
2023-01-04 14:56:07 +0300

    trim the files to remove json related things and filter routes

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,3 +1,3 @@
 class Inventory < ApplicationRecord
-  belongs_to :User
+  belongs_to :user
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  resources :inventories
-  get 'users/index'
   devise_for :users
-
   root "users#index"
   resources :users
+  # inventories
+  resources :inventories, only: [:index, :show, :destroy]
 end
