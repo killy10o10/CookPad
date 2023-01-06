a17d2934cf77e238e62a60a14881a7092aee0240
kibirige <kibirigejohn005@gmail.com>
2023-01-06 18:28:14 +0300

    fix a bug when deleting a food item

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,6 +1,6 @@
 class Inventory < ApplicationRecord
   belongs_to :user
-  # has_many :inventory_foods, foreign_key: 'food_id'
+  has_many :inventory_foods, foreign_key: 'food_id'
 
   validates :name, presence: true
   validates :description, presence: true
