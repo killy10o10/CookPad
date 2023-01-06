b7b026a9fa1d7b374943f0f065af40346bddda5e
Elfin Git <musongoletracy@gmail.com>
2023-01-06 11:13:52 +0200

    add index, show and public page for recipes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'users/index'
   devise_for :users
   get '/public_recipes', to: 'recipes#public'
+  # get '/new_recipe', to 'recipes#new', as: 'new_recipe'
 
   root "users#index"
   resources :users
